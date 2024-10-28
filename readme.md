4d19ac76b01d5a1c38a7f33138b7f6dced7cf0d1
hmunozt78 <hmunozt@gmail.com>
2024-10-28 12:14:29 -0300

    Todo solucionado, permisos asignados y requeridos segun corresponde

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Practica de Consolidacion Modulo 6
+
+Para clonar este proyecto, ejecute el siguiente comando
+
+git clone https://github.com/hmunozt78/proyecto_vehiculos_django.git
+
--- vehiculo/views.py
+++ vehiculo/views.py
@@ -6,17 +6,29 @@ from .forms import VehiculoForm
 from django.contrib.auth.decorators import login_required, permission_required 
 from django.views.generic import ListView
 from django.contrib.auth.mixins import AccessMixin, LoginRequiredMixin, UserPassesTestMixin, PermissionRequiredMixin
+from django.shortcuts import redirect
 
 
 
 # Create your views here.
-
+#LOS DECORADORES SON SOLO PARA FUNCIONES
 #@login_required(login_url='/usuarios/login/')
-#@permission_required('vehiculo.visualizar_catalogo', raise_exception=False, login_url='/')    
-class VehiculoListView(ListView):
+#@permission_required('vehiculo.visualizar_catalogo', raise_exception=False, login_url='/')   
+ 
+class VehiculoListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = Vehiculo
     template_name = "vehiculo/lista_vehiculos.html"
     context_object_name = 'vehiculos'
+    
+    permission_required = 'vehiculo.visualizar_catalogo'
+    
+    def handle_no_permission(self):
+        if self.request.user.is_authenticated:
+            messages.info(self.request,"Usted no tiene permisos")
+            return redirect('home')
+        else:
+            messages.info(self.request,"Primero debe identificarse")
+            return redirect('login')
 
 @login_required(login_url='/usuarios/login/')
 @permission_required('vehiculo.add_vehiculo', raise_exception=False, login_url='/')
